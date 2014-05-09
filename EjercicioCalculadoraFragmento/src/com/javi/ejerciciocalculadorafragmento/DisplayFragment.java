da7ab34fe31b3b16a60d55bdf7e05e93f9873251
JaviInf <jinfanteizk@gmail.com>
2014-05-09 12:31:20 +0200

    añadidos fragmentos.java

--- EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/DisplayFragment.java
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/DisplayFragment.java
@@ -6,18 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.TextView;
 
 
 public class DisplayFragment extends Fragment {
+	
+	TextView display;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_display, container, false);
+		View view=inflater.inflate(R.layout.fragment_display, container, false);
+		display= (TextView) view.findViewById(R.id.editText1);
+		return  view;
 	}
-
-	
 
 	
 
--- EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/NumerosFragment.java
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.javi.ejerciciocalculadorafragmento;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,13 +10,35 @@ import android.view.ViewGroup;
 
 public class NumerosFragment extends Fragment {
 	
+	public interface InterfazTeclado {
+		 public void numeroPulsado(String digito);
+		 public void operacionPulsado(String operadorNuevo);
+		 public String calcularOperacion();
+		 
+	}
+	
+	private InterfazTeclado interfazTeclado;
+	private int numeros[]={R.id.num0, R.id.num1, R.id.num2, R.id.num3, R.id.num4, R.id.num5, R.id.num6, R.id.num7, R.id.num8, R.id.num9};
+	private int operadores[]={R.id.opBorrar, R.id.opX, R.id.opPunto, R.id.opMenos, R.id.opMas, R.id.opIgual, R.id.opDiv};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_numeros, container, false);
+		View view=inflater.inflate(R.layout.fragment_numeros, container, false);
+		
+		return view;
 	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+	  super.onAttach(activity);
+	try {
+	    interfazTeclado=(InterfazTeclado)activity;
+	  } catch (ClassCastException e) {
+	    throw new ClassCastException(activity.toString() +
+	              " must implement OnSeasonSelectedListener");
+	} }
 
 	
 }
