f63a49933bcaa4adde908bb9cce53331fc58e799
JaviInf <jinfanteizk@gmail.com>
2014-05-09 12:50:58 +0200

    añadidos listener en el TecladoFragment y implementados metdos de interfazTeclado en el main activity

--- EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/MainActivity.java
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/MainActivity.java
@@ -1,16 +1,38 @@
 package com.javi.ejerciciocalculadorafragmento;
 
+import com.javi.ejerciciocalculadorafragmento.NumerosFragment.InterfazTeclado;
+import android.support.v4.app.Fragment;
 import android.app.Activity;
-
 import android.os.Bundle;
 
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements InterfazTeclado {
+	Calculadora calculadora;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		calculadora= new Calculadora();
+	}
+
+	@Override
+	public void numeroPulsado(String digito) {
+		// TODO Auto-generated method stub
+		calculadora.numeroPulsado(digito);
+		
+	}
+
+	@Override
+	public void operacionPulsado(String operadorNuevo) {
+		// TODO Auto-generated method stub
+		calculadora.operacionPulsado(operadorNuevo);
+		
+	}
+
+	@Override
+	public String calcularOperacion() {
+		// TODO Auto-generated method stub
+		return calculadora.calcularOperacion();
 	}
 
 
--- EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/NumerosFragment.java
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/NumerosFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class NumerosFragment extends Fragment {
@@ -14,12 +15,12 @@ public class NumerosFragment extends Fragment {
 		 public void numeroPulsado(String digito);
 		 public void operacionPulsado(String operadorNuevo);
 		 public String calcularOperacion();
-		 
 	}
 	
-	private InterfazTeclado interfazTeclado;
+	private InterfazTeclado actividad;
 	private int numeros[]={R.id.num0, R.id.num1, R.id.num2, R.id.num3, R.id.num4, R.id.num5, R.id.num6, R.id.num7, R.id.num8, R.id.num9};
-	private int operadores[]={R.id.opBorrar, R.id.opX, R.id.opPunto, R.id.opMenos, R.id.opMas, R.id.opIgual, R.id.opDiv};
+	private int operadores[]={R.id.opBorrar, R.id.opX, R.id.opPunto, R.id.opMenos, R.id.opMas, R.id.opDiv};
+	private int igual=R.id.opIgual;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +28,40 @@ public class NumerosFragment extends Fragment {
 		// Inflate the layout for this fragment
 		View view=inflater.inflate(R.layout.fragment_numeros, container, false);
 		
+		for(int i=0; i<numeros.length;i++)
+		{
+			final Button boton = (Button) view.findViewById(numeros[i]);
+			boton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					String numeroBoton = boton.getText().toString();
+					actividad.numeroPulsado(numeroBoton);	
+				}
+			});
+		}
+		
+		
+		//listener para los botones de operaciones
+		for(int i=0; i<operadores.length;i++)
+		{
+			final Button boton = (Button) view.findViewById(operadores[i]);
+			boton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					String operadorBoton = boton.getText().toString();
+					actividad.operacionPulsado(operadorBoton);
+				}
+			});	
+		}
+		final Button boton=(Button) view.findViewById(igual);
+		boton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				actividad.calcularOperacion();
+			}
+		});
 		return view;
 	}
 	
@@ -34,10 +69,10 @@ public class NumerosFragment extends Fragment {
 	public void onAttach(Activity activity) {
 	  super.onAttach(activity);
 	try {
-	    interfazTeclado=(InterfazTeclado)activity;
+	    actividad=(InterfazTeclado)activity;
 	  } catch (ClassCastException e) {
 	    throw new ClassCastException(activity.toString() +
-	              " must implement OnSeasonSelectedListener");
+	              " must implement InterfazTeclado");
 	} }
 
 	
