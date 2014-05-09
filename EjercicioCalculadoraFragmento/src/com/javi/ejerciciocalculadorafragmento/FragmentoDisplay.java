85247a2f63ae55f45c6cce9fdc142f1d06898e1f
JaviInf <jinfanteizk@gmail.com>
2014-05-09 09:45:36 +0200

    Primer commit añadidos dos fragmentos desde la clase

---
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/FragmentoDisplay.java
@@ -0,0 +1,7 @@
+package com.javi.ejerciciocalculadorafragmento;
+
+import android.app.Fragment;
+
+public class FragmentoDisplay extends Fragment {
+
+}
---
+++ EjercicioCalculadoraFragmento/src/com/javi/ejerciciocalculadorafragmento/FragmentoTeclado.java
@@ -0,0 +1,7 @@
+package com.javi.ejerciciocalculadorafragmento;
+
+import android.app.Fragment;
+
+public class FragmentoTeclado extends Fragment {
+
+}
