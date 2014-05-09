8aa51f73ed02bcd14839f956b9717a263cfec107
JaviInf <jinfanteizk@gmail.com>
2014-05-09 13:10:21 +0200

    Version 2 Calculadora

---
+++ CalculadoraFragmentos/src/com/javi/calculadorafraudulenta/CientificaFragment.java
@@ -0,0 +1,7 @@
+package com.javi.calculadorafraudulenta;
+
+import android.app.Fragment;
+
+public class CientificaFragment extends Fragment{
+
+}
---
+++ CalculadoraFragmentos/src/com/javi/calculadorafraudulenta/DisplayFragment.java
@@ -0,0 +1,18 @@
+package com.javi.calculadorafraudulenta;
+
+import com.example.calculadorafraudulenta.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DisplayFragment extends Fragment{
+	 @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                             Bundle savedInstanceState) {
+	        // Inflate the layout for this fragment
+	        return inflater.inflate(R.layout.teclado_fragment, container, false);
+	    }
+}
