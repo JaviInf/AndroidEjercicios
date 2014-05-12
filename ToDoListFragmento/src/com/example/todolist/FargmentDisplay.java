17d4a874ab7fc5f5fdc7ea1cee5dc7d6b873cfab
JaviInf <jinfanteizk@gmail.com>
2014-05-12 09:43:31 +0200

    Añadiendo listener en FragmentDisplay

--- ToDoListFragmento/src/com/example/todolist/FargmentDisplay.java
+++ ToDoListFragmento/src/com/example/todolist/FargmentDisplay.java
@@ -1,13 +1,22 @@
 package com.example.todolist;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FargmentDisplay extends Fragment {
-
+	
+	public interface InterfazDisplay {
+        public void obtenerTexto(String pTexto);
+    }
+	
+	private InterfazDisplay mainActivity;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -15,7 +24,32 @@ public class FargmentDisplay extends Fragment {
 	                         Bundle savedInstanceState) {
 	  // Create, or inflate the Fragment's UI, and return it.
 	  // If this Fragment has no UI then return null.
-	  return inflater.inflate(R.layout.fragment_display, container, false);
+	 View v= inflater.inflate(R.layout.fragment_display, container, false);
+	 
+	  EditText cajaTexto = (EditText) v.findViewById(R.id.editText1);
+	  Button botonMas= (Button) v.findViewById(R.id.anadir);
+	  
+	  botonMas.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	  
+	 return v;
 	}
+	
+	public void onAttach(Activity activity){
+		super.onAttach(activity);
+		try {
+            mainActivity = (InterfazDisplay) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + "debe implementar InterfazDisplay");
+        }
+    }
+		
 	
 }
--- ToDoListFragmento/src/com/example/todolist/MainActivity.java
+++ ToDoListFragmento/src/com/example/todolist/MainActivity.java
@@ -2,23 +2,24 @@ package com.example.todolist;
 
 import java.util.ArrayList;
 
+import com.example.todolist.FargmentDisplay.InterfazDisplay;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+
+
+
+
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.os.Build;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements InterfazDisplay{
 	
 	private ArrayList<String> listado;
 	private ArrayAdapter<String> adaptador;
@@ -30,26 +31,34 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-			listado= new ArrayList<String>();
-			adaptador= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listado);
-			cajaTexto=(EditText) findViewById(R.id.cajaTexto);
-			listadoView=(ListView) findViewById(R.id.lista);
-			listadoView.setAdapter(adaptador);			
-			cajaTexto.setOnKeyListener(new View.OnKeyListener() {
-				@Override
-				public boolean onKey(View v, int keyCode, KeyEvent event) {
-					// TODO Auto-generated method stub
-					if(event.getAction()==KeyEvent.ACTION_DOWN){
-						if(event.getAction()==KeyEvent.KEYCODE_DPAD_CENTER || (keyCode==KeyEvent.KEYCODE_ENTER)){
-							listado.add(0, cajaTexto.getText().toString());
-							adaptador.notifyDataSetChanged();
-							cajaTexto.setText("");
-							return true;
-						}
-					}
-					return false;
-				}
-			});
+		//FragmentManager
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.add(R.id.containerDisplay, new FargmentDisplay());
+		fragmentTransaction.commit();
+		
+		listado= new ArrayList<String>();
+		adaptador= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listado);
+		
+	
+		
+//			listadoView=(ListView) findViewById(R.id.lista);
+//			listadoView.setAdapter(adaptador);			
+//			cajaTexto.setOnKeyListener(new View.OnKeyListener() {
+//				@Override
+//				public boolean onKey(View v, int keyCode, KeyEvent event) {
+//					// TODO Auto-generated method stub
+//					if(event.getAction()==KeyEvent.ACTION_DOWN){
+//						if(event.getAction()==KeyEvent.KEYCODE_DPAD_CENTER || (keyCode==KeyEvent.KEYCODE_ENTER)){
+//							listado.add(0, cajaTexto.getText().toString());
+//							adaptador.notifyDataSetChanged();
+//							cajaTexto.setText("");
+//							return true;
+//						}
+//					}
+//					return false;
+//				}
+//			});
 	}
 
 	
@@ -76,6 +85,14 @@ public class MainActivity extends Activity {
 	     // Restore UI state from the savedInstanceState.
 	     // This bundle has also been passed to onCreate.
 	     listado.addAll(savedInstanceState.getStringArrayList("LISTADO"));
+	}
+
+
+	@Override
+	public void obtenerTexto(String nuevaEntrada) {
+		// TODO Auto-generated method stub
+		listado.add(nuevaEntrada);
+	     adaptador.notifyDataSetChanged();
 	}
 
 }
