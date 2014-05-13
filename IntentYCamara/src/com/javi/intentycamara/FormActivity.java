6d5e4771cf18d2e87756421a0c26df415ee759fc
JaviInf <jinfanteizk@gmail.com>
2014-05-13 10:48:33 +0200

    Intent entre MainActivity y FormActivity. Ida y vueltas de datos

--- IntentYCamara/src/com/javi/intentycamara/FormActivity.java
+++ IntentYCamara/src/com/javi/intentycamara/FormActivity.java
@@ -2,29 +2,48 @@ package com.javi.intentycamara;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class FormActivity extends Activity {
-
+public static final String MESSAGE="resultado";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_form);
 
-		EditText cajaTexto=(EditText) findViewById(R.id.cajaTexto);
-		TextView texto=(TextView) findViewById(R.id.texto);
+		final EditText cajaTexto=(EditText) findViewById(R.id.cajaTexto);
+		final TextView texto=(TextView) findViewById(R.id.texto);
 		Button botonAceptar = (Button) findViewById(R.id.aceptar);
 		Button botonCancelar = (Button) findViewById(R.id.cancelar);
 		
-		Bundle bundle = getIntent().getExtras();
-		texto.setText(bundle.getString(MainActivity.entrada));
+		final Intent intent=getIntent();
+		Bundle bundle = intent.getExtras();
+		
+		texto.setText(bundle.getString(MESSAGE));
 		
 		
+		botonAceptar.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				  intent.putExtra(MESSAGE,cajaTexto.getText().toString());
+				  setResult(RESULT_OK, intent);
+				  finish();
+			}
+		});
+		
+		botonCancelar.setOnClickListener(new OnClickListener() {
+			 
+	        @Override
+	        public void onClick(View v) {
+	           setResult(RESULT_CANCELED);
+	           finish();
+	        }
+	    });
 	}
 
 	
--- IntentYCamara/src/com/javi/intentycamara/MainActivity.java
+++ IntentYCamara/src/com/javi/intentycamara/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +19,9 @@ import android.os.Build;
 
 public class MainActivity extends Activity {
 	
-	public static final String entrada="entrada";
+		public static final String MESSAGE="resultado";
+	    private static final int RESULTADO = 0;
+	    private static final int FORMULARIO = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +30,57 @@ public class MainActivity extends Activity {
 
 			Button botonForm= (Button) findViewById(R.id.form);
 			final EditText cajaTexto= (EditText) findViewById(R.id.editText1);
-			TextView texto=(TextView) findViewById(R.id.cajamain);
+			
 			ImageView imagen= (ImageView) findViewById(R.id.imageView1);
 			
 			
 			botonForm.setOnClickListener(new View.OnClickListener() {
 				  public void onClick(View view) {
 					  Intent intent=new Intent(MainActivity.this, FormActivity.class);
-					  intent.putExtra(entrada, cajaTexto.getText().toString());
-					  startActivity(intent);
-				finish();
+					  intent.putExtra(MESSAGE, cajaTexto.getText().toString());
+					  startActivityForResult(intent, FORMULARIO);
+					  cajaTexto.setText("");
+				
 				} });
-		
 	}
 
-	
+//	@Override
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//	    super.onActivityResult(requestCode, resultCode, data);
+//	    TextView texto=(TextView) findViewById(R.id.cajamain);
+//	    if (resultCode == Activity.RESULT_CANCELED) {
+//	  
+//	        
+//	    } else if (resultCode==Activity.RESULT_OK) {
+//	   
+//	        String resultado = data.getExtras().getString("RESULTADO");
+//	        switch (requestCode) {
+//	        case FORMULARIO:
+//	            texto.setText(resultado);
+//	            break;       
+//	        }
+//	    }
+//	}
+	public void onActivityResult(int requestCode,
+	        int resultCode,
+	        Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	     
+	    TextView texto=(TextView) findViewById(R.id.cajamain);
+	     switch(requestCode) {
+	     case(FORMULARIO):
+	         if (resultCode == Activity.RESULT_OK)
+	            {
+	                String message = data.getStringExtra(FormActivity.MESSAGE);
+	                texto.setText(message);
+	            }
+	         else if(resultCode == Activity.RESULT_CANCELED)
+	            {
+	              
+	            }
+	     break;
+	      
+	     }
+	     
+	}
 }
