2f379867f168b1f284796c307cfc40e60dcfb36f
JaviInf <jinfanteizk@gmail.com>
2014-05-14 13:10:39 +0200

    añadidos receivers al androidManifest y codigo en el onRecibe

--- IntentYCamaraVersion2/src/com/javi/intentycamara/MyRecibidor.java
+++ IntentYCamaraVersion2/src/com/javi/intentycamara/MyRecibidor.java
@@ -3,14 +3,25 @@ package com.javi.intentycamara;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
-
-public class MyRecibidor extends BroadcastReceiver{
+public class MyRecibidor extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
-		String action=intent.getAction();
+		String action = intent.getAction();
+
+		if (action.equals("android.provider.Telephony.SMS_RECEIVED")) {
+			Log.d("RECIVER", "SMS entrante en el telefono");
+
+		} else if (action.equals("android.intent.action.PHONE_STATE")) {
+			Log.d("RECIVER", "LLamada entrante en el telefono");
+		}
+	 else if (action.equals("android.intent.action.AIRPLANE_MODE")) {
+		Log.d("RECIVER", "Modo avion detectado");
+	}
 	}
 
 }
