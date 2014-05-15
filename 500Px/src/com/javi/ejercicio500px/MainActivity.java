da8569baafb3fd5b082be1c9a9029169f22d38ff
JaviInf <jinfanteizk@gmail.com>
2014-05-15 10:25:53 +0200

    creacion de thread, ejecucion de soonsulta, procesamiento de JSON

--- 500Px/src/com/javi/ejercicio500px/MainActivity.java
+++ 500Px/src/com/javi/ejercicio500px/MainActivity.java
@@ -1,16 +1,23 @@
 package com.javi.ejercicio500px;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
@@ -19,15 +26,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		 Log.d("TAG", " ejecucion de on create");
+		 
 		Button boton = (Button) findViewById(R.id.consultaJson);
 		boton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
+				 Log.d("TAG", " creacion de un nuevo thread");
 				Thread t = new Thread(new Runnable() {
 					public void run() {
+						 Log.d("TAG", " ejecucion de un nuevo thread");
 						realizarConsulta();
 					}
-
 				});
 				t.start();
 			}
@@ -35,6 +44,7 @@ public class MainActivity extends Activity {
 	}
 	
 	public void realizarConsulta() {
+		 Log.d("TAG", "se esta realizando la consulta");
 		String myFeed = "http://www.arkaitzgarro.com/android/photos.json";
 		try {
 		  URL url = new URL(myFeed);
@@ -44,8 +54,10 @@ public class MainActivity extends Activity {
 		HttpURLConnection httpConnection = (HttpURLConnection)connection;
 		int responseCode = httpConnection.getResponseCode();
 		if (responseCode == HttpURLConnection.HTTP_OK) {
+			Log.d("TAG", "CONSULTA SATISFACTORIA");
 		  InputStream in = httpConnection.getInputStream();
-		  processStream(in);
+		  procesarConsulta(in);
+		  
 		}
 		}
 		catch (MalformedURLException e) {
@@ -57,8 +69,45 @@ public class MainActivity extends Activity {
 		
 	}
 
-	private void processStream(InputStream in) {
-		// TODO Auto-generated method stub
+	private void procesarConsulta(InputStream in) {
+		Log.d("TAG", "empieza procesamiento de respuesta");
+		 BufferedReader br = new BufferedReader(new InputStreamReader(in));
+         StringBuilder sb = new StringBuilder();
+         String line = null;
+          
+         try {
+			while ((line = br.readLine()) != null) {
+			        sb.append(line + "\n");
+			    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+          
+         Log.d("TAG",sb.toString());
+         try {
+			JSONObject json= new JSONObject(sb.toString());
+			procesareJSON(json);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void procesareJSON(JSONObject json) {
+		 String[] imagenes;
+		 try {
+			JSONArray fotos=(JSONArray)json.getJSONArray("photos");
+			//  Log.d("TAG","fotos.....:   "+fotos);
+			imagenes= new String[fotos.length()];
+			for(int i=0; i<fotos.length(); i++){
+				JSONObject imagen= fotos.getJSONObject(i);
+				 imagenes[i]=(String) imagen.get("image_url");
+                 Log.i("TAG","imagen en la posicion array "+i+","+imagenes[i]);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
