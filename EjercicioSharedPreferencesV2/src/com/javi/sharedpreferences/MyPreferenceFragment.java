ba51ae850cd9c39872ec996bb9cd21d63a8b0376
JaviInf <jinfanteizk@gmail.com>
2014-05-19 09:41:01 +0200

    EartquakesApp preferencias

--- EjercicioSharedPreferencesV2/src/com/javi/sharedpreferences/MyPreferenceFragment.java
+++ EjercicioSharedPreferencesV2/src/com/javi/sharedpreferences/MyPreferenceFragment.java
@@ -1,20 +1,12 @@
 package com.javi.sharedpreferences;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.preference.PreferenceFragment;
 
 public class MyPreferenceFragment  extends PreferenceFragment { 
 	public void onCreate(Bundle savedInstanceState) { 
-        super.onCreate(savedInstanceState); addPreferencesFromResource(R.xml.userpreferenceheaders);
+        super.onCreate(savedInstanceState); addPreferencesFromResource(R.xml.userpreferences);
     }
 	
 }
