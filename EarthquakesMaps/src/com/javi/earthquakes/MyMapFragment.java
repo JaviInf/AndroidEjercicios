b97cf4684ef5ad614f49482269cd2b0264407331
JaviInf <jinfanteizk@gmail.com>
2014-05-30 13:17:18 +0200

    añadido terremotos en el mapa

--- EarthquakesMaps/src/com/javi/earthquakes/MyMapFragment.java
+++ EarthquakesMaps/src/com/javi/earthquakes/MyMapFragment.java
@@ -1,40 +1,76 @@
 package com.javi.earthquakes;
 
+import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MyMapFragment extends MapFragment  implements
 LoaderCallbacks<Cursor>{
 	
+	private static final int LOADER_ID1 = 5;
 	private GoogleMap map;
+	private String[] result_columns = new String[] {
+			MyContentProvider.LAT, MyContentProvider.LONG, MyContentProvider.ID };
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
 		map = this.getMap();
-		
+		 LoaderManager lm = getLoaderManager();
+		    lm.initLoader(LOADER_ID1, null, this);
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 
 	@Override
-	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		SharedPreferences prefs=PreferenceManager
+				.getDefaultSharedPreferences(getActivity());
+		String where = MyContentProvider.MAGNITUDE + ">=?";
+		String whereArgs[] = { prefs.getString("magnitud_terremotos", "5") };
+		CursorLoader loader = new CursorLoader(getActivity(),
+				MyContentProvider.CONTENT_URI, result_columns, where,
+				whereArgs, MyContentProvider.TIME + " DESC");
+		return loader;
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
-		// TODO Auto-generated method stub
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		   while (cursor.moveToNext()) {
+			    
+	        
+	             
+	             int QUAKE_LOCATION_LAT_INDEX = cursor.getColumnIndexOrThrow(EarthquakesDBOpenHelper.LAT);         
+	             float latQuake = cursor.getFloat(QUAKE_LOCATION_LAT_INDEX);  
+	             
+	             int QUAKE_LOCATION_LONG_INDEX = cursor.getColumnIndexOrThrow(EarthquakesDBOpenHelper.LONG);             
+	             float longQuake= cursor.getFloat(QUAKE_LOCATION_LONG_INDEX);
+	             Log.d("MyMapFragmnet Lat", ""+latQuake);
+	             Log.d("MyMapFragmnet Long", ""+longQuake);
+            
+	          //	 this.crearMarcador(LatQuake, LongQuake);
+		   }
+		   }
 		
+
+	private void crearMarcador(float latQuake, float longQuake) {
+		MarkerOptions marker = new MarkerOptions()
+		   .position(new LatLng(latQuake, longQuake));
+		map.addMarker(marker);	
 	}
 
 	@Override
